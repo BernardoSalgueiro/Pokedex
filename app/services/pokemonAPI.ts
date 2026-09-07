ae776d84080b92615c37dc26b354008bcf379bfd
Bernardo Guimarães Salgueiro <bernardogsalgueiro11@gmail.com>
2026-09-07 11:22:01 -0300

    Criação da pasta `/services` e do arquivo `pokemonAPI.ts` responsável por se comunicar com a API.

---
+++ app/services/pokemonAPI.ts
@@ -0,0 +1,17 @@
+import { error } from "console";
+import { PokemonListResponse } from "../types/pokemon";
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+export async function getPokemons(
+    limit: number,
+    offset: number
+): Promise<PokemonListResponse> {
+    const response = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
+
+    if (!response.ok) {
+        throw new Error("Erro ao buscar Pokémon.");        
+    }
+
+    return response.json();
+}
